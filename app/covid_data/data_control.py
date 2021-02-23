1ec19eb1b083f8f3e1f4414c9d6473442ba3579a
O-Jun-S <junsannnn3562@icloud.com>
2021-02-23 20:05:20 +0900

    fixed data_control

--- app/covid_data/data_control.py
+++ app/covid_data/data_control.py
@@ -24,13 +24,14 @@ class DataControl:
     def update_date(self):
         self.the_day = datetime.now(JST) - timedelta(days=1)
         self.the_day_key = f"{self.the_day.year}/{self.the_day.month}/{self.the_day.day}"
-        print(self.the_day_key)
     
     def get_positive_data(self):
         return int(self.positive_data[self.positive_data["日付"] == self.the_day_key]["PCR 検査陽性者数(単日)"])
     
     def get_death_data(self):
-        before_the_day_key = (self.the_day - timedelta(days=1)).strftime("%Y/%-m/%-d")
+        before_the_day = (self.the_day - timedelta(days=1))
+        before_the_day_key = f"{before_the_day.year}/{before_the_day.month}/{before_the_day.day}"
+
         the_day_death = int(self.death_data[self.death_data["日付"] == self.the_day_key]["死亡者数"])
         the_day_before_yesterday_death = int(self.death_data[self.death_data["日付"] == before_the_day_key]["死亡者数"])
         return the_day_death - the_day_before_yesterday_death
@@ -40,6 +41,5 @@ class DataControl:
         return pcr_num
     
     def get_patient_data(self):
-        print(self.patient_data)
         patient_num = int(self.patient_data[self.patient_data["日付"] == self.the_day_key]["入院治療を要する者"])
         return patient_num
